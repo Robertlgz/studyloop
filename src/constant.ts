cbe029a721426953764d8bd3ba3d084bbea35783
Robertlgz <132150653+Robertlgz@users.noreply.github.com>
2026-08-15 09:11:58 +0800

    Add src/constant.ts

---
+++ src/constant.ts
@@ -0,0 +1,13 @@
+export const SEARCH_ICON = "book";
+export const SEARCH_PANEL_VIEW = "sl-search-panel";
+export const LEARN_PANEL_VIEW = "sl-learn-panel";
+export const STAT_VIEW_TYPE = "sl-stat";
+export const DATA_PANEL_VIEW = "sl-data-panel";
+export const READING_VIEW_TYPE = "sl-reading";
+export const REVIEW_QUEUE_VIEW = "sl-review-queue";
+
+export const STATUS_MAP = ["ignore", "learning", "familiar", "known", "learned"];
+export const STATUS_LABELS = ["忽略", "学习中", "熟悉", "掌握", "精通"];
+
+export const FRONT_MATTER_KEY = "langr";
+export const HISTORY_MAX = 50;
